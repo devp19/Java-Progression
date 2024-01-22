f6ac8339571b633a58900c72fa84340aed46245e
Dev Patel <devptl841806@gmail.com>
2024-01-21 23:01:48 -0500

    Update ScannerTest.java

--- CPS209 Lab Questions/ScannerTest.java
+++ CPS209 Lab Questions/ScannerTest.java
@@ -11,16 +11,14 @@ public class ScannerTest
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner("5.42 integer 99 Hello World!");
-        //-----------Start below here. To do: approximate lines of code = 4
-        // 1. read a double number from scanner into a variable called x
+       
         double x = scanner.nextDouble();
-        //2. read a word (ie. String) into a variable word
+       
         String word = scanner.next();
-        //3. read an integer from the scanner into a variable called i
+      
         int i = scanner.nextInt();
-        //4. read the rest of the words into a variable called line
+     
         String line = scanner.nextLine();
-        //-----------------End here. Please do not remove this comment. Reminder: no changes outside the todo regions.
 
         System.out.println("------ Double Test ------");
         System.out.println("OUTPUT:   " + x);
