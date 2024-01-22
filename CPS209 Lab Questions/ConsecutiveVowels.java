a6936e565dc94674aaa8e3436363114075eb535e
Dev Patel <devptl841806@gmail.com>
2024-01-22 00:01:06 -0500

    Update ConsecutiveVowels.java

--- CPS209 Lab Questions/ConsecutiveVowels.java
+++ CPS209 Lab Questions/ConsecutiveVowels.java
@@ -24,15 +24,6 @@ public class ConsecutiveVowels
             String word = in.next();
             boolean hasConsVowels = false;
 
-            //-----------Start below here. To do: approximate lines of code = 10
-            //
-            // Begin by initially hasConsVowels to false
-            // Use a for loop and go through each character of the String word
-            // check if the character is one of 'a' 'e' 'i' 'o' 'u'
-            // if it is, record (in a variable) the index where this occurred
-            // if another vowel is encountered, check to see if this new index is one more than the stored index (eg. index 3 then index 4 or index 7 then index 8)
-            // if so, set hasConsVowels to true. If not, update the index
-            
             boolean currentVowel = false;
             boolean previousVowel = false;
 
@@ -69,20 +60,7 @@ public class ConsecutiveVowels
                 previousVowel = currentVowel;
 
             }
-                // Check each letter of word to see if a vowel,
-                // when comparing characters don't forget to use ==
-                // Hint: use class String method charAt() to get the next character
-                // e.g. word.charAt(3) returns a char at position 3 in the string word
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            //-----------------End here. Please do not remove this comment. Reminder: no changes outside the todo regions.
+        
             System.out.println("------ Test " + test + " ------");
             if (hasConsVowels)
             {
